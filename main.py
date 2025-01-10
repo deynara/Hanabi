a3d4f6fdfd93932e1dc204c2c9f108be3634baa9
deynara <REDACTED>
2025-01-10 09:32:17 +0100

    initial framework

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Hanabi!")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    pygame.display.init()
+    while True:
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: #make quitbutton actually work
+                return
+        screen.fill((0,0,0)) #fill screen black
+
+if __name__ == "__main__":
+    main()
